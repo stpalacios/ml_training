783b62f351ec091cdf7c10021d42865bbdb4ed68
DrPFR <dsbc2020@users.noreply.github.com>
2020-03-27 14:45:58 -0500

    COVID-19 update

--- notebooks/Examples/coronavirus/matlab/Stock_Market_Analysis/stock_recovery.m
+++ notebooks/Examples/coronavirus/matlab/Stock_Market_Analysis/stock_recovery.m
@@ -3,14 +3,13 @@ SnP500 = readtable('HistoricalPrices_SnP500_2000_2020.csv');
 NASDAQ = readtable('HistoricalPrices_NASDAQ_2000_2020.csv');
 
 figure(1)
-plot(DJIA.Date,DJIA.Close,...
-    NASDAQ.Date,NASDAQ.Close,...
-    SnP500.Date,SnP500.Close,...
-    'LineWidth',2)
+plot(DJIA.Date,DJIA.Close,'r.-',...
+    NASDAQ.Date,NASDAQ.Close,'g.-',...
+    SnP500.Date,SnP500.Close,'b.-')
 datetick('x', 'mm-dd-yy', 'keepticks')
-legend('DJIA','NASDAQ','S&P 500')
+legend('DJIA','NASDAQ','S\&P 500','Interpreter','latex')
 xtickangle(45)
-title('Recovery of stock prices after national event.')
+title('Recovery of stock prices after national event.','Interpreter','latex')
 xlim([datetime('03/20/0000') datetime('05/01/0020')])
 
 d1 = datetime('09/10/0001');
@@ -18,13 +17,15 @@ d2 = datetime('11/09/0001');
 between(d1,d2,'Days')
 vline(d1,'r','9/11 Attack')
 vline(d2,'g')
-text(datetime('11/09/0001'),0.5e4,'Recovery = 61 days');
+text(datetime('11/09/0001'),0.5e4,'Recovery = 61 days','Interpreter','latex');
 
 d1 = datetime('10/08/0008');
 d2 = datetime('03/24/0010');
 between(d1,d2,'Days')
 vline(d1,'r','2008 Crash')
 vline(d2,'g')
-text(datetime('03/24/0010'),0.5e4,'Recovery = 532 days');
+text(datetime('03/24/0010'),0.5e4,'Recovery = 532 days','Interpreter','latex');
+
+dock_all_figures
 
 save_all_figs_OPTION('stock_recovery','png')
